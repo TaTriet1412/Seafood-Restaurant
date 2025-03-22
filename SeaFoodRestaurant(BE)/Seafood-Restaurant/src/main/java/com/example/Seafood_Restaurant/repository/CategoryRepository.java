a09ca2f1c026512f3e6ddcebd4d145abc81e48e3
S52200088 <52200088@student.tdtu.edu.vn>
2025-03-22 18:04:48 +0700

    BE: thêm các interface repository

---
+++ SeaFoodRestaurant(BE)/Seafood-Restaurant/src/main/java/com/example/Seafood_Restaurant/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.Seafood_Restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+}
---
+++ SeaFoodRestaurant(BE)/Seafood-Restaurant/src/main/java/com/example/Seafood_Restaurant/repository/DishImageRepository.java
@@ -0,0 +1,6 @@
+package com.example.Seafood_Restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DishImageRepository extends JpaRepository<DishImageRepository, Long> {
+}
