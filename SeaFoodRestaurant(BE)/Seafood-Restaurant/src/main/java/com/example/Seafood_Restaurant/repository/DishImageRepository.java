8c7f957e2638d1af6abadb1b2422ea3d4e3a5909
ngocquynh1912 <thangngocquynh1912@gmail.com>
2025-03-30 19:41:02 +0700

    BE: remove entity dishImage and dishIngredient (because we dont need it)

--- SeaFoodRestaurant(BE)/Seafood-Restaurant/src/main/java/com/example/Seafood_Restaurant/repository/DishImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Seafood_Restaurant.repository;
-
-import com.example.Seafood_Restaurant.entity.DishImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DishImageRepository extends JpaRepository<DishImage, Long> {
-}
